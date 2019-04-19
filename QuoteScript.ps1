23103243ea9c069fedd61ee8541e5e99d2c91b73
up741644 <47605643+up741644@users.noreply.github.com>
2019-04-19 05:12:38 +0100

    Add files via upload

---
+++ QuoteScript.ps1
@@ -0,0 +1,6 @@
+﻿# Set-ExecutionPolicy RemoteSigned
+
+(Get-Content "C:\Users\Alex\Documents\EngineeringProject\Tables\CHARTEVENTSTestQuotes.csv") |
+  % { ($_ -replace '^"|"$|(?<=\t)"|"(?=\t)', '') -replace '"', '' } |
+    
+    Set-Content "C:\Users\Alex\Documents\EngineeringProject\Tables\CHARTEVENTSTestQuotes.csv"
