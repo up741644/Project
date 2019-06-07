4d1463271fe3aa0ac5793557e1969fced3777276
up741644 <47605643+up741644@users.noreply.github.com>
2019-06-07 22:32:51 +0100

    Delete QuoteScript.ps1

--- QuoteScript.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Set-ExecutionPolicy RemoteSigned
-
-(Get-Content "C:\Users\Alex\Documents\EngineeringProject\Tables\CHARTEVENTSTestQuotes.csv") |
-  % { ($_ -replace '^"|"$|(?<=\t)"|"(?=\t)', '') -replace '"', '' } |
-    
-    Set-Content "C:\Users\Alex\Documents\EngineeringProject\Tables\CHARTEVENTSTestQuotes.csv"
